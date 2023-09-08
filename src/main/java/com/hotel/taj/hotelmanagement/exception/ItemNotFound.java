7626b8a648e22ece55d8ddedd3ef145e6d0ea72c
a-digraj <amithdd0@gmail.com>
2023-09-08 14:47:41 +0530

    first_commit

---
+++ src/main/java/com/hotel/taj/hotelmanagement/exception/ItemNotFound.java
@@ -0,0 +1,16 @@
+package com.hotel.taj.hotelmanagement.exception;
+
+public class ItemNotFound extends RuntimeException{
+	
+	private String message="item not found";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public ItemNotFound(String message) {
+		this.message = message;
+	}
+	
+	
+}
